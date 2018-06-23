8fde4d246d45887f9fb45594fd931a700b135728
Jerry Orta <jerryorta.dev@gmail.com>
2018-06-23 08:15:15 -0500

    feat(css): layout theme for resources

--- src/app/views/themes/references/resources/resources.component.ts
+++ src/app/views/themes/references/resources/resources.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, ViewEncapsulation } from '@angular/core';
 import { ROUTES } from '../../../../../configs/nav-items';
 import { IDataItem } from '../../../../../models/routes';
 
 @Component({
-  selector: 'resources',
-  templateUrl: './resources.component.html',
-  styleUrls: ['./resources.component.scss'],
-})
+             selector: 'resources',
+             templateUrl: './resources.component.html',
+             styleUrls: [ './resources.component.scss' ],
+             preserveWhitespaces: false,
+             encapsulation: ViewEncapsulation.None,
+             changeDetection: ChangeDetectionStrategy.OnPush,
+           })
 export class ResourcesComponent {
   data: IDataItem = ROUTES.themes.references.resources;
+
+  @HostBinding('class.layout-resources') bind = true;
 }
