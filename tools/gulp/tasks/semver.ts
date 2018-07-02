4728458f10b1a84f84460a84d350e3ef7acabc0b
Jerry Orta <jerryorta.dev@gmail.com>
2018-07-02 15:21:57 -0500

    bump

--- tools/gulp/tasks/semver.ts
+++ tools/gulp/tasks/semver.ts
@@ -9,6 +9,7 @@ import {
 } from '../util/inc_semver';
 const merge = require('merge-stream');
 const bump = require('gulp-bump');
+const git = require('gulp-git');
 import { argv  } from 'yargs';
 
 function updateCDK(version: string): any {
@@ -35,13 +36,37 @@ function updateRoot(version: string): any {
     .pipe(dest('./'));
 }
 
+const globs: any = [
+  './e2e/**/*',
+  './functions/**/*',
+  './projects/**/*',
+  './scripts/**/*',
+  './src/**/*',
+  './test/**/*',
+  './tools/**/*',
+  './*.*',
+];
+
+function add(): any {
+  return src('.')
+    .pipe(git.add());
+}
+
+function commit(version: string): any {
+  return src('.')
+    .pipe(git.commit(`Bumps to version ${version}`));
+}
+
 
 function updatePackages(version: string): any {
   return merge(updateCDK(version),
                updateMaterial(version),
                updateIcons(version),
                updateRoot(version),
-               copyAppPkg());
+               copyAppPkg(),
+               add(),
+               commit(version)
+  );
 }
 
 // example: yarn run bump --ver=8.0.1
