bc927fbc6f0ae40b45f2a6204c4a2e67e7ebc72d
Jerry Orta <jerryorta.dev@gmail.com>
2018-08-18 06:01:58 -0500

    feat(fn): karma

--- projects/uiux/fn/karma.conf.js
+++ projects/uiux/fn/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const {customLaunchers, platformMap} = require('../../../test/browser-providers');
 
 module.exports = function (config) {
   config.set({
@@ -12,20 +13,25 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    customLaunchers: customLaunchers,
+    browserConsoleLogOptions: {
+      terminal: true,
+      level: 'log'
+    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage'),
+      dir: require('path').join(__dirname, '../../../coverage/fn'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['dots'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessLocal'],
     singleRun: false
   });
 };
