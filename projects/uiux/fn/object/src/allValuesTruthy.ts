58d958417d9cfeb827b2ab591e8fdbb84e0f803c
Jerry Orta <jerryorta.dev@gmail.com>
2018-08-21 17:23:32 -0500

    refactor(fn): allValuesTruthy

---
+++ projects/uiux/fn/object/src/allValuesTruthy.ts
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright UIUX Engineering All Rights Reserved.
+ */
+
+import { isEmpty, isPlainObject, isTruthy } from '@uiux/fn/common';
+
+export function allValuesTruthy(object: any): boolean {
+  if (isPlainObject(object)) {
+    if (!isEmpty(object)) {
+      return Object.keys(object).reduce((_allPropsHaveValue: boolean, key: string) => {
+        if (!_allPropsHaveValue) {
+          return _allPropsHaveValue;
+        }
+        return isTruthy(object[key]);
+      }, true);
+    } else {
+      // object is empty
+      return false;
+    }
+  } else {
+    // not plain object
+    return false;
+  }
+}
