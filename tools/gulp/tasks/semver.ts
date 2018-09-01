8691d3c2826a25513216e6780c2c8693635647fe
Jerry Orta <jerryorta.dev@gmail.com>
2018-09-01 10:51:45 -0500

    feat(semver):

--- tools/gulp/tasks/semver.ts
+++ tools/gulp/tasks/semver.ts
@@ -10,15 +10,18 @@ import {
 const merge = require('merge-stream');
 const bump = require('gulp-bump');
 const git = require('gulp-git');
+const chalk = require('chalk');
 import { argv  } from 'yargs';
 
 function updateCDK(version: string): any {
+  console.log(chalk.green('cdk'));
   return src('./projects/uiux/cdk/package.json')
     .pipe(bump({version: version}))
     .pipe(dest('./projects/uiux/cdk/'));
 }
 
 function updateFN(version: string): any {
+  console.log(chalk.green('fn'));
   return src('./projects/uiux/fn/package.json')
     .pipe(bump({version: version}))
     .pipe(dest('./projects/uiux/fn/'));
@@ -26,36 +29,42 @@ function updateFN(version: string): any {
 
 
 function updateDAL(version: string): any {
+  console.log(chalk.green('dal'));
   return src('./projects/uiux/dal/package.json')
     .pipe(bump({version: version}))
     .pipe(dest('./projects/uiux/dal/'));
 }
 
 function updaterxjs(version: string): any {
+  console.log(chalk.green('rxjs'));
   return src('./projects/uiux/rxjs/package.json')
     .pipe(bump({version: version}))
     .pipe(dest('./projects/uiux/rxjs/'));
 }
 
 function updatesvc(version: string): any {
+  console.log(chalk.green('sevices'));
   return src('./projects/uiux/services/package.json')
     .pipe(bump({version: version}))
     .pipe(dest('./projects/uiux/services/'));
 }
 
 function updateMaterial(version: string): any {
+  console.log(chalk.green('material'));
   return src('./projects/uiux/material/package.json')
     .pipe(bump({version: version}))
     .pipe(dest('./projects/uiux/material/'));
 }
 
 function updateIcons(version: string): any {
+  console.log(chalk.green('icons'));
   return src('projects/uiux/icons/package.json')
     .pipe(bump({version: version}))
     .pipe(dest('./projects/uiux/icons/'));
 }
 
 function updateRoot(version: string): any {
+  console.log(chalk.green('package.json'));
   return src('./package.json')
     .pipe(bump({version: version}))
     .pipe(dest('./'));
