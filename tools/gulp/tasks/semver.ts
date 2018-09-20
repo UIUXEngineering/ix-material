ceca8b989572a35a5487db89805cd2a5e2605925
Jerry Orta <jerryorta.dev@gmail.com>
2018-09-20 03:53:13 -0500

    refactor(semver): update names

--- tools/gulp/tasks/semver.ts
+++ tools/gulp/tasks/semver.ts
@@ -14,7 +14,10 @@ const merge = require('merge-stream');
 const bump = require('gulp-bump');
 const LOG_COLOR = 'green';
 
-
+/**
+ * cdk
+ * @param version
+ */
 function updateCDK(version: string): any {
 
   return src('./projects/uiux/cdk/package.json')
@@ -23,21 +26,21 @@ function updateCDK(version: string): any {
     .pipe(dest('./projects/uiux/cdk/'));
 }
 
+/**
+ * d3
+ * @param version
+ */
 function updated3(version: string): any {
   return src('./projects/uiux/d3/package.json')
+    .pipe(logPipe('d3', LOG_COLOR))
     .pipe(bump({version: version}))
     .pipe(dest('./projects/uiux/d3/'));
 }
 
-function updateFN(version: string): any {
-
-  return src('./projects/uiux/fn/package.json')
-    .pipe(logPipe('fn', LOG_COLOR))
-    .pipe(bump({version: version}))
-    .pipe(dest('./projects/uiux/fn/'));
-}
-
-
+/**
+ * dal
+ * @param version
+ */
 function updateDAL(version: string): any {
 
   return src('./projects/uiux/dal/package.json')
@@ -46,30 +49,22 @@ function updateDAL(version: string): any {
     .pipe(dest('./projects/uiux/dal/'));
 }
 
-function updaterxjs(version: string): any {
+/**
+ * fn
+ * @param version
+ */
+function updateFN(version: string): any {
 
-  return src('./projects/uiux/rxjs/package.json')
-    .pipe(logPipe('rxjs', LOG_COLOR))
+  return src('./projects/uiux/fn/package.json')
+    .pipe(logPipe('fn', LOG_COLOR))
     .pipe(bump({version: version}))
-    .pipe(dest('./projects/uiux/rxjs/'));
+    .pipe(dest('./projects/uiux/fn/'));
 }
 
-function updatesvc(version: string): any {
-
-  return src('./projects/uiux/services/package.json')
-    .pipe(logPipe('services', LOG_COLOR))
-    .pipe(bump({version: version}))
-    .pipe(dest('./projects/uiux/services/'));
-}
-
-function updateMaterial(version: string): any {
-
-  return src('./projects/uiux/material/package.json')
-    .pipe(logPipe('material', LOG_COLOR))
-    .pipe(bump({version: version}))
-    .pipe(dest('./projects/uiux/material/'));
-}
-
+/**
+ * icons
+ * @param version
+ */
 function updateIcons(version: string): any {
 
   return src('projects/uiux/icons/package.json')
@@ -78,6 +73,46 @@ function updateIcons(version: string): any {
     .pipe(dest('./projects/uiux/icons/'));
 }
 
+/**
+ * material
+ * @param version
+ */
+function updateMaterial(version: string): any {
+
+  return src('./projects/uiux/material/package.json')
+    .pipe(logPipe('material', LOG_COLOR))
+    .pipe(bump({version: version}))
+    .pipe(dest('./projects/uiux/material/'));
+}
+
+/**
+ * rxjs
+ * @param version
+ */
+function updaterxjs(version: string): any {
+
+  return src('./projects/uiux/rxjs/package.json')
+    .pipe(logPipe('rxjs', LOG_COLOR))
+    .pipe(bump({version: version}))
+    .pipe(dest('./projects/uiux/rxjs/'));
+}
+
+/**
+ * services
+ * @param version
+ */
+function updatesvc(version: string): any {
+
+  return src('./projects/uiux/services/package.json')
+    .pipe(logPipe('services', LOG_COLOR))
+    .pipe(bump({version: version}))
+    .pipe(dest('./projects/uiux/services/'));
+}
+
+/**
+ * root
+ * @param version
+ */
 function updateRoot(version: string): any {
 
   return src('./package.json')
@@ -89,12 +124,12 @@ function updateRoot(version: string): any {
 function updatePackages(version: string): any {
   return merge(updateCDK(version),
                updated3(version),
-               updateFN(version),
                updateDAL(version),
+               updateFN(version),
+               updateIcons(version),
+               updateMaterial(version),
                updaterxjs(version),
                updatesvc(version),
-               updateMaterial(version),
-               updateIcons(version),
                updateRoot(version),
                copyAppPkg());
 }
