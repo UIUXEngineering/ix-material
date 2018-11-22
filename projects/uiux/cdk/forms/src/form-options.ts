8825fb3d05f5a2e47ebedcd5622597a3ae736f80
Jerry Orta <jerryorta.dev@gmail.com>
2018-11-22 12:15:37 -0600

    feat(form labels):

--- projects/uiux/cdk/forms/src/form-options.ts
+++ projects/uiux/cdk/forms/src/form-options.ts
@@ -1,4 +1,21 @@
-export const FORM_OPTIONS: any = {
+export interface LabelOptions {
+  ALWAYS: string;
+  NEVER: string;
+}
+
+export interface AppearanceOptions {
+  LEGACY: string;
+  STANDARD: string;
+  FILL: string;
+  OUTLINE: string;
+}
+
+export interface FormOptions {
+  FLOAT_LABEL: LabelOptions;
+  APPEARANCE: AppearanceOptions;
+}
+
+export const FORM_OPTIONS: FormOptions = {
   FLOAT_LABEL: {
     ALWAYS: 'always',
     NEVER: 'never',
