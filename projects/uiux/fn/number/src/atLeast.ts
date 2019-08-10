48971c38924c4c25255b4009feda3b8137ddf358
Jerry Orta <jerryorta.dev@gmail.com>
2019-08-09 22:24:10 -0500

    atLeast

---
+++ projects/uiux/fn/number/src/atLeast.ts
@@ -0,0 +1,15 @@
+import { isDefined } from './isDefined';
+import { isNumber } from './isNumber';
+
+export function atLeast(min: number): (val: number) => number {
+  if (!isDefined(min) || !isNumber(min)) {
+    throw(new Error('min provided must be a number'));
+  }
+  return function(val: number): number {
+    if (!isDefined(val) || !isNumber(val)) {
+      throw(new Error('Value provided must be a number'));
+    }
+
+    return val > min ? val : min;
+  };
+}
