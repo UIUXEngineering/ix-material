1987f2f0d9038bb059c252fa8b250ad29d2ab53d
Jerry Orta <jerryorta.dev@gmail.com>
2020-04-04 21:55:04 -0500

    feat(update):

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,16 +3,16 @@
 ROOT_DIR=$PWD;
 
 rm -rf dist/libs
-ng build fn
-ng build cdk
-ng build rxjs
-ng build device
-ng build firebase
-ng build services
-ng build dal
-ng build d3
+ng build fn --prod
+ng build cdk --prod
+ng build rxjs --prod
+ng build device --prod
+ng build firebase --prod
+ng build services --prod
+ng build dal --prod
+ng build d3 --prod
 #ng build material
-ng build ngrx
+ng build ngrx --prod
 
 #bash ./scripts/build-prebuilt-themes-mat.sh
 #bash ./scripts/build-app-themes.sh
