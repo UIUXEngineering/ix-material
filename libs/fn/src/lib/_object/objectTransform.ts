e8a98a1b1aedf2e921e44dfae72d993db1cc102f
Jerry Orta <jerryorta.dev@gmail.com>
2020-08-29 16:41:07 -0500

    feat(objecttransformmap): reverse src and target

--- libs/fn/src/lib/_object/objectTransform.ts
+++ libs/fn/src/lib/_object/objectTransform.ts
@@ -6,12 +6,18 @@ import { isPlainObject } from '../_common/isPlainObject';
 import { isEmpty } from '../_common/isEmpty';
 import { setIn } from '../_common/setIn';
 import { getIn } from '../_common/getIn';
+import { isString } from '../_common/isString';
 
-export function objectTransform(target: any, map: any): any {
-  if (isPlainObject(target) && isPlainObject(map)) {
-    if (!isEmpty(target) && !isEmpty(map)) {
+/**
+ * @deprecated
+ * @param srcObj
+ * @param map
+ */
+export function objectTransform(srcObj: any, map: any): any {
+  if (isPlainObject(srcObj) && isPlainObject(map)) {
+    if (!isEmpty(srcObj) && !isEmpty(map)) {
       return Object.keys(map).reduce((acc: any, key: any) => {
-        acc = setIn(acc, key, getIn(target, map[key], null));
+        acc = setIn(acc, key, getIn(srcObj, map[key], null));
         return acc;
       }, {});
     } else {
@@ -19,6 +25,28 @@ export function objectTransform(target: any, map: any): any {
       // objects
       return {};
     }
+  } else {
+    // plain _object is empty
+    return {};
+  }
+}
+
+export function objectTransformMap<T>(srcObj: any, map: any): any {
+  if (isPlainObject(srcObj) && isPlainObject(map) && !isEmpty(srcObj) && !isEmpty(map)) {
+    return Object.keys(map).reduce((tarObj: any, srcKey: string) => {
+      if (isString(map[srcKey])) {
+        const targetKey = map[srcKey];
+
+        tarObj = setIn(tarObj, targetKey, getIn(srcObj, srcKey, null));
+      } else if (Array.isArray(map[srcKey])) {
+        for (let i = 0; i < map[srcKey].length; i++) {
+          const targetKey = map[srcKey][i];
+          tarObj = setIn(tarObj, targetKey, getIn(srcObj, srcKey, null));
+        }
+      }
+
+      return tarObj;
+    }, {});
   } else {
     // plain _object is empty
     return {};
