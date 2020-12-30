05295ad9688d3bfb0629c7b6e9f7488827bcddcc
Jerry Orta <jerryorta.dev@gmail.com>
2020-12-30 05:22:47 -0600

    refactor(clonepipe): use rxjs map

--- libs/rxjs/src/lib/pipes/clone-pipe.ts
+++ libs/rxjs/src/lib/pipes/clone-pipe.ts
@@ -2,25 +2,16 @@
  * @license
  * Copyright UIUX Engineering All Rights Reserved.
  */
-import { Observable, Subscriber } from 'rxjs';
 import { clone } from '@uiux/fn';
+import { OperatorFunction } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-export function clonePipe<T>(): any {
-  return (source: Observable<T>): Observable<T> => {
-    return new Observable((observer: Subscriber<T>) => {
-      return source.subscribe({
-        next(x: any) {
-          if (x) {
-            observer.next(clone(x));
-          }
-        },
-        error(err) {
-          observer.error(err);
-        },
-        complete() {
-          observer.complete();
-        },
-      });
-    });
-  };
+export function clonePipe<T>(): OperatorFunction<T, T> {
+  return map((d: T) => {
+    try {
+      return clone(d);
+    } catch (e: any) {
+      return d;
+    }
+  });
 }
