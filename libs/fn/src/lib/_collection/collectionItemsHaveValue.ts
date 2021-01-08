cc5f45c04086bda3674a51578c64959c651fcbcc
Jerry Orta <jerryorta.dev@gmail.com>
2021-01-08 07:17:47 -0600

    chore(collectionitemshavevalue): add return tuype

--- libs/fn/src/lib/_collection/collectionItemsHaveValue.ts
+++ libs/fn/src/lib/_collection/collectionItemsHaveValue.ts
@@ -1,6 +1,6 @@
 import { hasValue } from '../_common/hasValue';
 
-export function collectionItemsHaveValue(collection: any[]) {
+export function collectionItemsHaveValue(collection: any[]): boolean {
   if (collection && collection.length > 0) {
     return collection.reduce((_allPropsHaveValue: boolean, item: any) => {
       if (!_allPropsHaveValue) {
